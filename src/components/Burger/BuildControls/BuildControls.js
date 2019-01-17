fe8c6da57225b67d3830035f4015a88f9e458608
gagaception <sharipov.aziz.sh@gmail.com>
2019-01-17 12:41:03 +0600

    Added remove ingredient with disabled button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -16,7 +16,9 @@ const buildControls = (props) => (
       <BuildControl
         key={ctr.label}
         label={ctr.label}
-        added={() => props.ingredientAdded(ctr.type)}/>
+        added={() => props.ingredientAdded(ctr.type)}
+        removed={() => props.ingredientRemoved(ctr.type)}
+        disabled={props.disabled[ctr.type]}/>
     ))}
   </div>
 );
