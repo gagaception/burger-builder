78639a272b570803e198dca1ed4c7804bf43bed3
gagaception <sharipov.aziz.sh@gmail.com>
2019-01-17 15:01:33 +0600

    Added price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -62,7 +62,8 @@ class BurgerBuilder extends Component {
         <BuildControls
           ingredientAdded={this.addIngredientsHandler}
           ingredientRemoved={this.removeIngredientsHandler}
-          disabled={disabledInfo} />
+          disabled={disabledInfo}
+          price={this.state.totalPrice} />
       </Aux>
     );
   }
